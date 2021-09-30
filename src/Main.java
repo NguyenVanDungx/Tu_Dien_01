76c00d9d10a2db601a54454dcdfd2e6541a77e60
NguyenVanDungx <20020192@vnu.edu.vn>
2021-09-30 07:35:17 +0700

    add 1 line nua

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Heelo Word");
         System.out.println("them cai nua");
+        System.out.println("Hello them ca nua");
     }
 }
